516ccf4eb37ccd866712f794912dae43346f879c
Faizal Vasaya <faizalvasaya@Faizals-Macbook.local>
2023-01-01 16:28:23 +0530

    feat: Basic http server

---
+++ http_server_app.py
@@ -0,0 +1,14 @@
+import http.server
+
+class HandleRequest(http.server.BaseHTTPRequestHandler):
+    
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+        self.wfile.write(bytes('<html><h2>Welcome to our first prometheus course</h2></html>','utf-8'))
+        self.wfile.close
+
+if __name__ == "__main__":
+    server = http.server.HTTPServer(('localhost',5000), HandleRequest)
+    server.serve_forever()
